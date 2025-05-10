42aa3eea5dab8ae05084241579175358276beea4
Curtis <125740245+CurtisCullenAWong@users.noreply.github.com>
2025-05-10 17:38:02 +0800

    environment variables

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
-  };
-}; 
+    api.cache(true)
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        ['dotenv-import', {
+          moduleName: '@env',
+          path: '.env.local',
+          allowUndefined: true,
+        }]
+      ]
+    }
+}
+  
--- lib/supabase.jsx
+++ lib/supabase.jsx
@@ -1,14 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'
 
-const supabaseUrl = 'https://szmquzyhjxjzvlzazyuz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bXF1enloanhqenZsemF6eXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNjI2NzMsImV4cCI6MjA2MTczODY3M30.1ou1FAO_ndGBU5Mrp_zTxr81OyR17Zb5YYI0JpK_7dg'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey,{
-    auth:{
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 })
