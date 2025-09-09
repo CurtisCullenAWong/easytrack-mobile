ef6ce5e457e6072d9e7e462b2b92f6ec6329e28f
Curtis <125740245+CurtisCullenAWong@users.noreply.github.com>
2025-09-09 22:04:16 +0800

    ADMIN FIX

--- lib/supabaseAdmin.jsx
+++ lib/supabaseAdmin.jsx
@@ -1,9 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } from '@env'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY,{
-    auth: {
-        autoRefreshToken: false,
-        persistSession: false
-    }
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 })
